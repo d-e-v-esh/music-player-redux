73d94d097fd3bab599bd9af32f0ae297eaba9fe9
d-e-v-esh <59534570+d-e-v-esh@users.noreply.github.com>
2020-12-07 19:07:36 +0530

    All States Working

--- src/store/Player.jsx
+++ src/store/Player.jsx
@@ -1,3 +1,28 @@
 // Action Types
 
-const PLAY_PAUSE_SONG;
+// const PLAY_PAUSE_SONG;
+
+// Player Reducer---------------------------------------
+
+import songData from "../data";
+const initState = {
+  allSongs: songData(), // If I run this data function then it is just going to return us the array of objects containing the song data
+  currentSong: [songData()[0]],
+  songPlayStatus: "PAUSED",
+  isLibraryOpen: false,
+  songInfo: [
+    {
+      currentTime: 0,
+      duration: 0,
+    },
+  ],
+};
+
+const playerReducer = (state = initState, action) => {
+  switch (action.type) {
+    default:
+      return { ...state };
+  }
+};
+
+export default playerReducer;
--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,5 +1,5 @@
 import { combineReducers } from "redux";
-import playerReducer from "./playerReducer";
+import playerReducer from "./Player";
 
 const rootReducer = combineReducers({
   player: playerReducer,
